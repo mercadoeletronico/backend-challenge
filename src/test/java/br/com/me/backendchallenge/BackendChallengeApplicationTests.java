1fba5301700efc7f0964324765132114df330400
Danilo Mesquita <danilofmesquita@gmail.com>
2020-12-19 08:02:23 -0300

    Projeto inicial

---
+++ src/test/java/br/com/me/backendchallenge/BackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.me.backendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
