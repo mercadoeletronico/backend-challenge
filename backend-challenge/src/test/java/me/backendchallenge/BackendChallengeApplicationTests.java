2675a506b929f411ad309bac58c7ad34ce10f8d0
Tibet Teixeira <tibetbrasil@gmail.com>
2020-12-28 23:30:59 -0300

    Starting project

---
+++ backend-challenge/src/test/java/me/backendchallenge/BackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.backendchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
