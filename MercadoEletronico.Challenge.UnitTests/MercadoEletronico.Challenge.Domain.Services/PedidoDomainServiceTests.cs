5e825da884ff3b45f299d872c2722b6056869bf1
Pedro Farias <pedro.barbosarf@gmail.com>
2021-04-19 10:31:41 -0300

    Unit testes fechados

--- MercadoEletronico.Challenge.UnitTests/MercadoEletronico.Challenge.Domain.Services/PedidoDomainServiceTests.cs
+++ MercadoEletronico.Challenge.UnitTests/MercadoEletronico.Challenge.Domain.Services/PedidoDomainServiceTests.cs
@@ -5,10 +5,7 @@ using MercadoEletronico.Challenge.Domain.Services.Implementations;
 using MercadoEletronico.Challenge.Domain.Services.Interfaces.Data_Access;
 using MercadoEletronico.Challenge.Util.Extensions;
 using Moq;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -17,32 +14,13 @@ namespace MercadoEletronico.Challenge.UnitTests.MercadoEletronico.Challenge.Doma
     public class PedidoDomainServiceTests
     {
         [Fact]
-        public async Task Aprovar() 
+        public async Task Aprovar_MustReturnAprovadoWhenQuantityAndValueMatch() 
         {
             // Arrange
             const string pedidoId = "ABC";
 
-            var pedido = new Pedido 
-            { 
-                Id = pedidoId,
-                Itens = new List<PedidoItem> 
-                {
-                    new()
-                    { 
-                        PedidoId = pedidoId,
-                        Id = "1",
-                        PrecoUnitario = 10,
-                        Qtd = 2
-                    },
-                    new()
-                    {
-                        PedidoId = pedidoId,
-                        Id = "2",
-                        PrecoUnitario = 5,
-                        Qtd = 1
-                    }
-                }
-            };
+            Pedido pedido = CreatePedidoMock(pedidoId);
+
             var repositoryMock = new Mock<IPedidoRepository>();
 
             var request = new StatusRequest 
@@ -63,6 +41,213 @@ namespace MercadoEletronico.Challenge.UnitTests.MercadoEletronico.Challenge.Doma
             Assert.Equal(pedidoId, response.Pedido);
             Assert.Single(response.Status);
             Assert.Contains(response.Status, status => status == StatusAprovacao.Aprovado.GetDescription());
+        }
+
+        [Fact]
+        public async Task Aprovar_MustReturnAprovadoValorMenorWhenQuantityMatchesAndValueIsLower()
+        {
+            // Arrange
+            const string pedidoId = "ABC";
+
+            Pedido pedido = CreatePedidoMock(pedidoId);
+
+            var repositoryMock = new Mock<IPedidoRepository>();
+
+            var request = new StatusRequest
+            {
+                Pedido = pedidoId,
+                Status = StatusAprovacao.Aprovado.GetDescription(),
+                ItensAprovados = 3,
+                ValorAprovado = 23
+            };
+
+            repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<string>())).ReturnsAsync(pedido);
+            var service = new PedidoDomainService(repositoryMock.Object);
+
+            // Act
+            var response = await service.AprovarPedido(request);
+
+            // Assert
+            Assert.Equal(pedidoId, response.Pedido);
+            Assert.Single(response.Status);
+            Assert.Contains(response.Status, status => status == StatusAprovacao.AprovadoValorMenor.GetDescription());
+        }
+
+        [Fact]
+        public async Task Aprovar_MustReturnAprovadoValorMaiorWhenQuantityMatchesAndValueIsHigher()
+        {
+            // Arrange
+            const string pedidoId = "ABC";
+
+            Pedido pedido = CreatePedidoMock(pedidoId);
+
+            var repositoryMock = new Mock<IPedidoRepository>();
+
+            var request = new StatusRequest
+            {
+                Pedido = pedidoId,
+                Status = StatusAprovacao.Aprovado.GetDescription(),
+                ItensAprovados = 3,
+                ValorAprovado = 37
+            };
+
+            repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<string>())).ReturnsAsync(pedido);
+            var service = new PedidoDomainService(repositoryMock.Object);
+
+            // Act
+            var response = await service.AprovarPedido(request);
+
+            // Assert
+            Assert.Equal(pedidoId, response.Pedido);
+            Assert.Single(response.Status);
+            Assert.Contains(response.Status, status => status == StatusAprovacao.AprovadoValorMaior.GetDescription());
+        }
+
+        [Fact]
+        public async Task Aprovar_MustReturnAprovadoQuantidadeMenorWhenValueMatchesAndQuantityIsLower()
+        {
+            // Arrange
+            const string pedidoId = "ABC";
+
+            Pedido pedido = CreatePedidoMock(pedidoId);
+
+            var repositoryMock = new Mock<IPedidoRepository>();
+
+            var request = new StatusRequest
+            {
+                Pedido = pedidoId,
+                Status = StatusAprovacao.Aprovado.GetDescription(),
+                ItensAprovados = 2,
+                ValorAprovado = 25
+            };
+
+            repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<string>())).ReturnsAsync(pedido);
+            var service = new PedidoDomainService(repositoryMock.Object);
+
+            // Act
+            var response = await service.AprovarPedido(request);
+
+            // Assert
+            Assert.Equal(pedidoId, response.Pedido);
+            Assert.Single(response.Status);
+            Assert.Contains(response.Status, status => status == StatusAprovacao.AprovadoQuantidadeMenor.GetDescription());
+        }
+
+        [Fact]
+        public async Task Aprovar_MustReturnAprovadoQuantidadeMenorWhenValueMatchesAndQuantityIsHigher()
+        {
+            // Arrange
+            const string pedidoId = "ABC";
+
+            Pedido pedido = CreatePedidoMock(pedidoId);
+
+            var repositoryMock = new Mock<IPedidoRepository>();
+
+            var request = new StatusRequest
+            {
+                Pedido = pedidoId,
+                Status = StatusAprovacao.Aprovado.GetDescription(),
+                ItensAprovados = 5,
+                ValorAprovado = 25
+            };
+
+            repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<string>())).ReturnsAsync(pedido);
+            var service = new PedidoDomainService(repositoryMock.Object);
+
+            // Act
+            var response = await service.AprovarPedido(request);
+
+            // Assert
+            Assert.Equal(pedidoId, response.Pedido);
+            Assert.Single(response.Status);
+            Assert.Contains(response.Status, status => status == StatusAprovacao.AprovadoQuantidadeMaior.GetDescription());
+        }
+
+        [Fact]
+        public async Task Aprovar_MustReturnAprovadoQuantidadeMaiorValorMaiorWhenBothAreHigher()
+        {
+            // Arrange
+            const string pedidoId = "ABC";
+
+            Pedido pedido = CreatePedidoMock(pedidoId);
+
+            var repositoryMock = new Mock<IPedidoRepository>();
+
+            var request = new StatusRequest
+            {
+                Pedido = pedidoId,
+                Status = StatusAprovacao.Aprovado.GetDescription(),
+                ItensAprovados = 5,
+                ValorAprovado = 30
+            };
+
+            repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<string>())).ReturnsAsync(pedido);
+            var service = new PedidoDomainService(repositoryMock.Object);
+
+            // Act
+            var response = await service.AprovarPedido(request);
+
+            // Assert
+            Assert.Equal(pedidoId, response.Pedido);
+            Assert.Equal(2, response.Status.Count);
+            Assert.Contains(response.Status, status => status == StatusAprovacao.AprovadoValorMaior.GetDescription());
+            Assert.Contains(response.Status, status => status == StatusAprovacao.AprovadoQuantidadeMaior.GetDescription());
+        }
+
+        [Fact]
+        public async Task Aprovar_MustReturnAprovadoQuantidadeMenorValorMenorWhenBothAreLower()
+        {
+            // Arrange
+            const string pedidoId = "ABC";
+
+            Pedido pedido = CreatePedidoMock(pedidoId);
+
+            var repositoryMock = new Mock<IPedidoRepository>();
+
+            var request = new StatusRequest
+            {
+                Pedido = pedidoId,
+                Status = StatusAprovacao.Aprovado.GetDescription(),
+                ItensAprovados = 2,
+                ValorAprovado = 11
+            };
+
+            repositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<string>())).ReturnsAsync(pedido);
+            var service = new PedidoDomainService(repositoryMock.Object);
+
+            // Act
+            var response = await service.AprovarPedido(request);
+
+            // Assert
+            Assert.Equal(pedidoId, response.Pedido);
+            Assert.Equal(2, response.Status.Count);
+            Assert.Contains(response.Status, status => status == StatusAprovacao.AprovadoValorMenor.GetDescription());
+            Assert.Contains(response.Status, status => status == StatusAprovacao.AprovadoQuantidadeMenor.GetDescription());
+        }
+
+        private static Pedido CreatePedidoMock(string pedidoId)
+        {
+            return new Pedido
+            {
+                Id = pedidoId,
+                Itens = new List<PedidoItem>
+                {
+                    new()
+                    {
+                        PedidoId = pedidoId,
+                        Id = "1",
+                        PrecoUnitario = 10,
+                        Qtd = 2
+                    },
+                    new()
+                    {
+                        PedidoId = pedidoId,
+                        Id = "2",
+                        PrecoUnitario = 5,
+                        Qtd = 1
+                    }
+                }
+            };
         }
     }
 }
